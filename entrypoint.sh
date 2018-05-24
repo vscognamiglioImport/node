1c7a7f8095878e1c08c36698a27ae5dfd6cb96bc
vscognamiglioImport <39514340+vscognamiglioImport@users.noreply.github.com>
2018-05-24 09:37:18 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 DOCUMENT_ROOT=${DOCUMENT_ROOT:-"/var/www/localhost/htdocs"}
 LOG=${LOG:-"/var/log/darkhttpd/access.log"}
 
-/usr/bin/darkhttpd $DOCUMENT_ROOT --log $LOG --port 80 --no-server-id
+/usr/bin/darkhttpd $DOCUMENT_ROOT --log $LOG --port 8888 --no-server-id
